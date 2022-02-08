bc63ddf1b79db7e5411d138808d6c9cfc8134fc3
Parth <parth.shiralkar@gmail.com>
2022-02-07 20:46:29 -0800

    Setup sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('images')
     eleventyConfig.addPassthroughCopy('admin')
+
+    eleventyConfig.addWatchTarget("./src/sass");
   
     const {
       DateTime
