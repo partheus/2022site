e1afb9df2e70a54fbf1827950c45965d0ab7672e
Parth <parth.shiralkar@gmail.com>
2022-03-14 21:59:21 -0700

    Update config files

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,13 @@ const packageVersion = require("./package.json").version;
 
 const { DateTime } = require("luxon");
 
+const fs = require("fs");
+const NOT_FOUND_PATH = "public/404.html";
+
+module.exports = function(eleventyConfig) {
+  
+};
+
 
 
 module.exports = function (eleventyConfig) {
@@ -30,6 +37,26 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/favicon.png");
   eleventyConfig.addPassthroughCopy("./src/oakbranch.png");
 
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          if (!fs.existsSync(NOT_FOUND_PATH)) {
+            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+          }
+
+          const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
 
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   url: process.env.URL || "http://localhost:8081",
   siteName: "Parth Shiralkar",
   siteDescription:
-    "Parth Shiralkar's personal website",
+    "Parth Shiralkar personal website",
   authorName: "Parth Shiralkar",
   // twitterUsername: "parthshiralkar", // no `@`
 };
